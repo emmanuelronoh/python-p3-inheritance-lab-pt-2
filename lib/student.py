537f1e20f814ec474c887b522b87403a0c865a1e
emmanuel ronoh <eronoh036@gmail.com>
2024-09-04 13:44:52 +0300

    first commit

--- lib/student.py
+++ lib/student.py
@@ -1,5 +1,23 @@
+# student.py
+
+import sys
+
 class Student:
-    pass
+    def __init__(self, first_name=None, last_name=None):
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def hello(self):
+        print("Hey there! I'm so excited to learn stuff.")
+
+    def raise_hand(self):
+        print("Pick me!")
 
 class ChattyStudent(Student):
-    pass
+    def hello(self):
+        super().hello()
+        print("How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...")
+
+    def raise_hand(self):
+        for _ in range(10):
+            print("Pick me!")
